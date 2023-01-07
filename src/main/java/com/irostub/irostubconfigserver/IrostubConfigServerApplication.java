cb567890d95657b23d0dbf48ee967aa92621cc7a
irostub <irostub@gmail.com>
2023-01-07 16:49:14 +0900

    Init commit

---
+++ src/main/java/com/irostub/irostubconfigserver/IrostubConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.irostub.irostubconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class IrostubConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrostubConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irostub/irostubconfigserver/IrostubConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irostub.irostubconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrostubConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
